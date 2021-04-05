7544585c509093e1e8149a7afa04132ae6a748d2
Jesper de Jong <jespdj@gmail.com>
2021-04-05 11:29:35 +0200

    Added source-map-loader.

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,11 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.js$/,
+                enforce: 'pre',
+                use: ['source-map-loader']
+            },
+            {
                 test: /\.css$/i,
                 use: ['style-loader','css-loader'],
             },
